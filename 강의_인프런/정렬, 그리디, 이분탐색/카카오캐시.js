768748fb2d5bfc22d06fcbb549f84fecd4dc447d
leeinsong <insong1216@gmail.com>
2021-04-22 22:04:41 +0900

    Add: 카카오 캐시 풀이 수정

--- 강의_인프런/정렬, 그리디, 이분탐색/카카오캐시.js
+++ 강의_인프런/정렬, 그리디, 이분탐색/카카오캐시.js
@@ -1,21 +1,62 @@
-function solution(n, arr) {
-  var answer = [];
-  let i, j;
-  for (i = 0; i < arr.length; i++) {
-    if (answer.includes(arr[i]) === false) {
-      if (answer.length > n - 1) {
-        answer.pop();
+// function solution(n, arr) {
+//   var answer = [];
+//   let i, j;
+//   for (i = 0; i < arr.length; i++) {
+//     if (answer.includes(arr[i]) === false) {
+//       if (answer.length > n - 1) {
+//         answer.pop();
+//       }
+//       answer.unshift(arr[i]);
+//     } else {
+//       var tempIndex = answer.indexOf(arr[i]);
+//       var temp = answer[tempIndex];
+//       for (j = tempIndex - 1; j >= 0; j--) {
+//         answer[j + 1] = answer[j];
+//       }
+//       answer[j + 1] = temp;
+//     }
+//   }
+//   return answer;
+// }
+
+// function solution(size, arr) {
+//   //answer 라는 array 의 크기와 해당 요소 값을 0 으로 초기화
+//   let answer = Array.from({ length: size }, () => 0);
+//   arr.map((data) => {
+//     // position 을 -1 로 초기화
+//     // miss 난 경우에는 position 에 해당 i 값을 저장
+//     let position = -1;
+//     for (let i = 0; i < size; i++) if (data === answer[i]) position = i;
+//     if (position === -1) {
+//       for (let i = size - 1; i >= 1; i--) {
+//         answer[i] = answer[i - 1];
+//       }
+//     } else {
+//       for (let i = position; i >= 1; i--) {
+//         answer[i] = answer[i - 1];
+//       }
+//     }
+//       // 반복을 방지하기 위해 맨 밑으로
+//     answer[0] = data;
+//   });
+//   return answer;
+// }
+function solution(size, arr) {
+  let answer = Array.from({ length: size }, () => 0);
+  arr.map((data) => {
+    let position = -1;
+    for (let i = 0; i < arr.length; i++) if (answer[i] === data) position = i;
+    if (position === -1) {
+      for (let i = size - 1; i >= 1; i--) {
+        answer[i] = answer[i - 1];
       }
-      answer.unshift(arr[i]);
     } else {
-      var tempIndex = answer.indexOf(arr[i]);
-      var temp = answer[tempIndex];
-      for (j = tempIndex - 1; j >= 0; j--) {
-        answer[j + 1] = answer[j];
+      for (let i = position; i >= 1; i--) {
+        answer[i] = answer[i - 1];
       }
-      answer[j + 1] = temp;
     }
-  }
+    answer[0] = data;
+  });
   return answer;
 }
 
